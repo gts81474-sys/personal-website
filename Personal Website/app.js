4b1e353e2cf112887736768e113721167745c4fd
gts81474-sys <gts81474@gmail.com>
2025-10-30 02:22:42 +0800

    Delete Personal Website directory

--- Personal Website/app.js
+++
@@ -1,9 +0,0 @@
-let nav = document.querySelector("nav");
-
-window.addEventListener("scroll", function () {
-  if (window.scrollY == 0) {
-    nav.style.boxShadow = "none";
-  } else {
-    nav.style.boxShadow = "0px 10px 6px -6px #777";
-  }
-});
